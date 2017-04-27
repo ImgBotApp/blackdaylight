b1712045209e7f44ceb36db1ddcd86392fc6bcaa
AJ Johnson <ajilityjohnson@gmail.com>
2017-04-27 02:50:18 -0500

    added wp data

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,81 @@
 
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+  <div id="app" v-if="categoryResponse.length">
+    <h1>blackdaylight</h1>
+    <div v-if="category.id !== 1 && category.name !== 'home'" v-bind:class="category.slug" v-for="category in categoryResponse">
+      <h2 v-on:click="toggle(clicked)">
+        {{ category.name }}
+      </h2>
+      <ul :class="{selected: clicked}" >
+        <li v-if="category.id === post.categories[0]" v-for="post in postResponse" v-html="post.content.rendered"></li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
 
 export default {
   name: 'app',
   components: {
-    Hello
+  },
+  data () {
+    return {
+      categoryResponse: [],
+      postResponse: [],
+      clicked: false
+    }
+  },
+  methods: {
+
+    fetchCategories: function () {
+      fetch('http://50.87.249.59/~blackday/wp-json/wp/v2/categories?per_page=100')
+      .then(
+        function (response) {
+          if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' +
+              response.status)
+            return
+          }
+
+          // Examine the text in the response
+          response.json().then(function (data) {
+            this.categoryResponse = data
+          }.bind(this))
+        }.bind(this)
+      )
+      .catch(function (err) {
+        console.log('Fetch Error :-S', err)
+      })
+    },
+    fetchPosts: function () {
+      fetch('http://50.87.249.59/~blackday/wp-json/wp/v2/posts?per_page=100')
+      .then(
+        function (response) {
+          if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' +
+              response.status)
+            return
+          }
+
+          // Examine the text in the response
+          response.json().then(function (data) {
+            console.log(data)
+            this.postResponse = data
+          }.bind(this))
+        }.bind(this)
+      )
+      .catch(function (err) {
+        console.log('Fetch Error :-S', err)
+      })
+    },
+    toggle: function (clicked) {
+      this.clicked = !clicked
+    }
+  },
+  mounted: function () {
+    this.fetchCategories()
+    this.fetchPosts()
   }
 }
 </script>
@@ -24,6 +87,20 @@ export default {
   -moz-osx-font-smoothing: grayscale
   text-align: center
   color: #2c3e50
-  margin-top: 60px
-
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+#app > div
+  border: 1px solid #eee
+  box-sizing: border-box
+  margin: 1%
+  padding: 1%
+  width: 48%
+h1
+  flex: 0 1 100%
+  font-size: 2em
+  margin: 2%
+h2
+  font-size: 1.5em
+  margin: 1%
 </style>
